c0d00e1533125f1e0c1cc2b905ec8caedacde250
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2020-08-12 16:21:00 +0530

    Add frontend

---
+++ build/precache-manifest.02e22827c0f0f4c92b25c46e5146b28c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ca0e8ff76a376f8df2a99ee613455f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813161b33dae4e7906de",
+    "url": "/static/js/main.0c7d78c5.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
