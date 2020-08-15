54b154a73b0d389ea21ea418896c23a69312ba65
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2020-08-15 11:45:39 +0530

    Connect backend with the database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const person = require('./models/person')
+const { response } = require('express')
 
 const app = express()
 
@@ -17,64 +20,40 @@ app.use(morgan(
 
 const PORT = process.env.PORT || 3001
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 5
-    }
-]
-
-const generateId = () => {
-    let new_id = 1
-    
-    do { new_id = Math.floor(Math.random()*1000000) }
-    while (persons.find((n) => n.id === new_id))
-    
-    return new_id
-}
-
 app.get('/api/persons', (request, response) => {
     console.log("Responding to getAll on the main route")
-    response.json(persons)
+    person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = persons.find((n) => n.id === id)
-    if(note) 
-        res.json(note)
-    else return res.status(404).end()
+app.get('/api/persons/:id', (req, res, next) => {
+    person.findById(req.params.id).then(note => {        
+        if(note) res.json(note)
+        else res.status(404).end()
+    }).catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
+    person.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
+})
+
+app.get('/info', (req, res, next) => {
     console.log("Fetching info")
-    res.send(`<p>We have information about ${persons.length} people</p><p>${Date()}</p>`)
+    person.find({}).then(persons => {
+        res.send(`<p>We have information about ${persons.length} people</p><p>${Date()}</p>`)
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter((n) => n.id !== id)
-
-    return res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    person.findByIdAndRemove(req.params.id).then(
+        result => res.status(204).end()
+    ).catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const contact = req.body
     if(!contact.name || !contact.number) {
         console.log("Received malformed contact")
@@ -83,19 +62,33 @@ app.post('/api/persons', (req, res) => {
 
     console.log(`Received ${contact.name} ${contact.number}`)
 
-    if(persons.find((p) => contact.name === p.name)) {
-        console.log(`${contact.name} already exists`)
-        return res.status(400).json({"error": `${contact.name} already exists`})
-    }
-
     const newEntry = {
-        ...contact,
-        id: generateId()
+        name: contact.name,
+        number: contact.number
     }
 
-    persons = persons.concat(newEntry)
-    res.json(newEntry)    
+    const entry = new person(newEntry)
+    entry.save()
+        .then(savedEntry => {res.json(savedEntry)})
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: "Unknown endpoint"})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: "Malformed ID"})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
