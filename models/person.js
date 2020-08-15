e02c26414dd015f772ecfd41c4b77779b3ddbfb5
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2020-08-15 12:42:54 +0530

    expand validation

---
+++ models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const uniqueValidator  = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => {console.log("Connected to mongodb")})
+    .catch(error => {
+        console.log(`Error while connecting to mongoDB ${error.message}`)
+    })
+
+const phoneSchema = new mongoose.Schema({
+    name: {type: String, unique: true, required: true, minlength: 3},
+    number: {type: String, required: true, minlength: 8}
+})
+
+phoneSchema.set('toJSON', {
+    transform: (doc, returned) => {
+        returned.id = returned._id.toString(),
+        delete returned._id
+        delete returned.__v
+    }
+})
+
+module.exports = mongoose.model('People', phoneSchema)
